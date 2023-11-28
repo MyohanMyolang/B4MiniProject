a5d055fc583715daaac4115476c7632e977f2f22
Myolang <myohanmyolang@gmail.com>
2023-11-28 19:14:22 +0900

    template

---
+++ js/Firestore.js
@@ -0,0 +1,61 @@
+import db from "./FirestoreInit.js"
+import { collection, doc, updateDoc ,getDocs, setDoc, query, where,deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+
+const memberRef = collection(db, "member");
+
+export const setMember = async (data) => {
+  try{
+    await setDoc(doc(memberRef), data, { merge: true });
+  }
+  catch (e) {
+    alert("데이터 저장에 실패하였습니다.");
+    console.error(e);
+  }
+}
+
+/**
+ * 
+ * @param {string} key 해당 Filed의 Key
+ * @param {string} value 해당 Filed의 Value
+ * @returns 성공시 Data 실패시 null
+ */
+export const getMember = async (key, value) => {
+  const q = query(memberRef, where(key, "==", value))
+  
+  const result = {
+    id: null,
+    data: null,
+  };
+  const snap = await getDocs(q);
+  snap.forEach((doc) => {
+    result.id = doc.id;
+    result.data = doc.data();
+  })
+  
+  return result;
+}
+
+export const getAllMember = async () => {
+  const result = [];
+  const snap = await getDocs(memberRef);
+  snap.forEach((doc) => {
+    result.push(
+      {
+        id: doc.id,
+        ...doc.data()
+      }
+    );
+  })
+
+  return result;
+}
+
+export const updateMember = async (id, data) => {
+  console.log(id);
+  await updateDoc(doc(memberRef, id), data)
+}
+
+export const deleteMember = async (id) => {
+  await deleteDoc(doc(db, "member", id))
+}
---
+++ js/RenderUtil.js
@@ -0,0 +1,10 @@
+import Firestore from "./Firestore.js";
+
+/**
+ * TODO: 수정 버튼을 누를 시, 입력 가능한 부분은 Input 태그로 변환 시키고, 입력되어 있는 Data를 value로 사용하는 기능 추가
+ *
+ */
+
+const init = () => {};
+
+init();
