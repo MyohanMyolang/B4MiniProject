9ac321581f47f1098b5d1546c636674617f830a1
Umin <adorecamus@gmail.com>
2023-11-29 19:12:13 +0900

    fix: 확인 버튼 보임 오류 수정

--- js/comments.js
+++ js/comments.js
@@ -3,17 +3,23 @@ import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "https://
 
 // 방명록 등록하기
 export async function postComments(doc) {
-    await addDoc(collection(db, "4dev"), doc);
-            alert('등록되었습니다!');
-            window.location.reload();
+    await addDoc(collection(db, "comments"), doc);
+    alert('등록되었습니다!');
+    window.location.reload();
 }
 
 // 방명록 목록 가져오기
 export async function getCommentsList() {
-    let docs = await getDocs(collection(db, "4dev"));
+    let docs = await getDocs(collection(db, "comments"));
+    if (docs.size == 0) return; 
+
+    $('#commentsbox').show();
+
     let no = 0;
     docs.forEach((doc) => {
+        // 버튼 구분 위해 숫자 붙여줌
         let docNo = ++no;
+
         // 수정, 삭제 위해 id 가져옴
         let docId = doc.id;
     
@@ -35,6 +41,9 @@ export async function getCommentsList() {
         </div>`;
         $('#commentsbox').append(temp_html);
     
+        // 확인 버튼 숨김
+        $('#confirmbtn'+docNo).hide();
+
         // 수정 버튼 클릭 이벤트
         $('#editingbtn'+docNo).click(async function () {
             editComments(docNo, docId, password);
@@ -51,16 +60,16 @@ export async function getCommentsList() {
 async function editComments(docNo, docId, password) {
     let input = prompt('수정하시려면 비밀번호를 입력하세요.');
     if (input === password) {
-        $('#content' + docNo).attr('contenteditable', 'true');
-        $('#content' + docNo).css('border', '1px solid grey');
+        $('#content'+docNo).attr('contenteditable', 'true');
+        $('#content'+docNo).css('border', '1px solid grey');
 
-        $('#editingbtn' + docNo).hide();
-        $('#confirmbtn' + docNo).show();
+        $('#editingbtn'+docNo).hide();
+        $('#confirmbtn'+docNo).show();
 
         // 확인 버튼 클릭 이벤트
-        $('#confirmbtn' + docNo).click(async function () {
-            const docRef = await doc(db, "4dev", docId);
-            await updateDoc(docRef, { content: $('#content' + docNo).text() });
+        $('#confirmbtn'+docNo).click(async function () {
+            const docRef = await doc(db, "comments", docId);
+            await updateDoc(docRef, { content: $('#content'+docNo).text() });
             alert('수정되었습니다!');
             window.location.reload();
         })
@@ -71,9 +80,8 @@ async function editComments(docNo, docId, password) {
 async function deleteComments(docId, password) {
     let input = prompt('삭제하시려면 비밀번호를 입력하세요.');
     if (input === password) {
-        const docRef = await doc(db, "4dev", docId);
+        const docRef = await doc(db, "comments", docId);
         await deleteDoc(docRef);
-
         alert('삭제되었습니다!')
         window.location.reload();
     }
